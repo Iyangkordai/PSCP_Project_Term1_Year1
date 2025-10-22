61a4a3cc3ce2de4f81e08758aa6cae61f341959b
Iyangkordai <suphichaya.khanereo@gmail.com>
2025-10-22 17:05:38 +0700

    add delete list funtion

--- static/script.js
+++ static/script.js
@@ -1,7 +1,6 @@
 const template_cost = document.getElementById("cost_list_template")
 let cost_div = document.getElementById("cost_list_div")
 function new_list_cost(){
-    console.log("hello")
     const child = template_cost.content.cloneNode(true)
     cost_div.appendChild(child)
     cost_div.lastElementChild.scrollIntoView({
@@ -9,3 +8,17 @@ function new_list_cost(){
         block: 'nearest'
     })
 }
+
+cost_div.addEventListener('click', function(event){
+
+    const delete_bt = event.target.closest('.delete_bt')
+    if (delete_bt){
+        event.preventDefault()
+
+        const want_row = delete_bt.closest("#list_div")
+        if (want_row && cost_div.childElementCount > 1){
+            console.log("found want row ")
+            want_row.remove()
+        }
+    }
+})
