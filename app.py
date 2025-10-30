0da54746eae25a783072080a16aa005fd9405198
pnz902 <ananyayaya.ay07@gmail.com>
2025-10-30 17:21:51 +0700

    make path '/submit' and function sendData
    to calculate the input and return to summary page

--- app.py
+++ app.py
@@ -6,6 +6,56 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route("/submit",methods=['POST'])
+def sendData():
+    if request.method == 'POST':
+        #ต้นทุนวัตถุดิบ
+        name_1 = request.form.getlist('name_1')
+        cost_price = request.form.getlist('cost_price', type=float)
+        cost_amount = request.form.getlist('cost_amount', type=float)
+        cost_used = request.form.getlist('cost_used', type=float)
+
+        grand_total = 0.0
+        all_cost_details = []
+        for name, price, amount, used in zip(name_1, cost_price, cost_amount, cost_used):
+            
+            row_total = 0.0
+            
+            # ป้องกันการหารด้วย 0 (ZeroDivisionError)
+            if amount > 0:
+                # สูตรคำนวณต้นทุนของแถวนี้
+                # (ราคา / ปริมาณทั้งหมด) * จำนวนที่ใช้
+                row_total = (price / amount) * used
+            
+            # เพิ่มต้นทุนแถวนี้ เข้าไปในยอดรวมทั้งหมด
+            grand_total += row_total
+            all_cost_details.append({
+                'name': name,
+                'price': price,
+                'amount': amount,
+                'used': used,
+                'row_total': row_total
+            })
+
+        #ค่าเสื่อม
+        name_2 = request.form.getlist('name_2')
+        expenses = request.form.getlist('expenses', type=float)
+        product_amount = request.form.getlist('product_amount', type=float)
+
+        #บรรจุภัณฑ์
+        package_list = request.form.getlist('package_list')
+        package_price = request.form.getlist('package_price', type=float)
+        package_amount = request.form.getlist('package_amount', type=float)
+
+        #แรงงาน
+
+        return render_template(
+            'summary.html', 
+            cost_total=grand_total,
+            cost_data=all_cost_details
+        )
+
+
 if __name__ == "__main__":
     app.run(debug=True)
 
