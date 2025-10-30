52c20b242c82e990bf2f7f9ed05a8e9da3523a6b
Apich01 <68070202@kmitl.ac.th>
2025-10-30 18:06:52 +0700

    Correct Table 1 in calculations

--- app.py
+++ app.py
@@ -9,15 +9,13 @@ def index():
 @app.route("/submit",methods=['POST'])
 def sendData():
     if request.method == 'POST':
-        #ต้นทุนวัตถุดิบ
-        cost_list = request.form.getlist('cost_list')
+        #ต้นทุนวัตถุดิบs
         cost_price = request.form.getlist('cost_price', type=float)
         cost_amount = request.form.getlist('cost_amount', type=float)
         cost_used = request.form.getlist('cost_used', type=float)
 
-        grand_total = 0.0
-        all_cost_details = []
-        for name, price, amount, used in zip(cost_list, cost_price, cost_amount, cost_used):
+        cost_total = 0.0
+        for price, amount, used in zip(cost_price, cost_amount, cost_used):
             
             row_total = 0.0
             
@@ -28,22 +26,13 @@ def sendData():
                 row_total = (price / amount) * used
             
             # เพิ่มต้นทุนแถวนี้ เข้าไปในยอดรวมทั้งหมด
-            grand_total += row_total
-            all_cost_details.append({
-                'name': name,
-                'price': price,
-                'amount': amount,
-                'used': used,
-                'row_total': row_total
-            })
+            cost_total += row_total
 
         #ค่าเสื่อม
-        depreciation_list = request.form.getlist('depreciation_list')
         expenses = request.form.getlist('expenses', type=float)
         product_amount = request.form.getlist('product_amount', type=float)
 
         #บรรจุภัณฑ์
-        package_list = request.form.getlist('package_list')
         package_price = request.form.getlist('package_price', type=float)
         package_amount = request.form.getlist('package_amount', type=float)
 
@@ -51,8 +40,7 @@ def sendData():
 
         return render_template(
             'summary.html', 
-            cost_total=grand_total,
-            cost_data=all_cost_details
+            cost_total=cost_total
         )
 
 
