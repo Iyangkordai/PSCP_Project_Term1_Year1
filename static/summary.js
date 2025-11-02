708ad29c156dfda9b77921d281f06164f64e8307
Saymhai <151415377+Saymhai@users.noreply.github.com>
2025-11-02 14:29:58 +0700

    edit manual slicing function, css, html(tam dai leaw najaa)

--- static/summary.js
+++ static/summary.js
@@ -1,5 +1,9 @@
 var modal_manual = document.getElementById("manual")
 var btn_manual = document.getElementById("user_manual_bt")
+const carousel = document.getElementById('carousel')
+const slides = document.querySelectorAll('.slide')
+const prevBtn = document.getElementById('prevBtn')
+const nextBtn = document.getElementById('nextBtn')
 
 // แสดงผลคู่มือการใช้งานเว็บ
 btn_manual.onclick = function() {
@@ -11,3 +15,30 @@ modal_manual.onclick = function(event) {
         modal_manual.classList.remove("show");
     }
 }
+
+//web sliding
+let currentIndex = 0;
+const totalSlides = slides.length;
+function updateCarousel() {
+    carousel.style.transform = `translateX(-${currentIndex * 100}%)`;
+}
+
+nextBtn.addEventListener('click', function() {
+    currentIndex = currentIndex + 1;
+        if (currentIndex >= totalSlides) {
+            currentIndex = 0;
+        }
+
+        updateCarousel();
+    });
+
+prevBtn.addEventListener('click', function() {
+    currentIndex = currentIndex - 1;
+    if (currentIndex < 0) {
+        currentIndex = totalSlides - 1;
+    }
+
+    updateCarousel();
+});
+
+updateCarousel();
